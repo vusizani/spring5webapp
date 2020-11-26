6e7935106d150afc6efd5866fdb67735207633b5
nerdeaux <vusi.sizani@gmail.com>
2020-11-26 12:53:02 +0200

    implement equals and hashCode methods.

--- src/main/java/vusi/springframework/spring5webapp/Author.java
+++ src/main/java/vusi/springframework/spring5webapp/Author.java
@@ -56,4 +56,19 @@ public class Author {
     public void setBooks(Set<Book> books) {
         this.books = books;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
--- src/main/java/vusi/springframework/spring5webapp/Book.java
+++ src/main/java/vusi/springframework/spring5webapp/Book.java
@@ -58,4 +58,19 @@ public class Book {
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
